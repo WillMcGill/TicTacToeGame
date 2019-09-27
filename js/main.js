350ac5604e0bbdd25357400691609e87352125ea
Will McGIll <willmcgill@Makiyas-iPhone.localdomain>
2019-09-27 10:07:32 -0400

    started win checker

--- js/main.js
+++ js/main.js
@@ -37,16 +37,17 @@ function clickHandler() {
             document.getElementById("turnIndicator").innerHTML = "X's Turn";
             player = 1;
         }
-
+        winChecker();
     }
     else { }
 
 }
 
 function clickReset() {
-    console.log('reset');
+    //console.log('reset');
     for (var i = 0; i < size * size; i++) {
-        console.log(i);
+        //console.log(i);
+        player = 1;
         document.getElementById(i).innerHTML = "";
         document.getElementById("turnIndicator").innerHTML = "X Goes First";
     }
@@ -67,21 +68,18 @@ function createUI() {
     var count = 0;
     for (var i = 0; i < size; i++) {
         var newRow = document.createElement("div");
-        newRow.className = "row no-gutters";
+        newRow.className = "row no-gutters justify-content-md-center";
 
 
         for (var j = 0; j < size; j++) {
             var newCol = document.createElement("div");
-            newCol.className = "col border py-5";
+            newCol.className = "col col-lg-4 border py-5";
             newCol.id = count;
 
             var test = document.createTextNode(" ");
             newCol.addEventListener('click', clickHandler);
+            newCol.setAttribute("style", "height: 100px;")
             newRow.appendChild(newCol);
-
-            newCol.appendChild(test);
-            //newCol.addEventListener('click', console.log('click'));
-            //newCol.appendChild(square);
             count++;
         }
         app.appendChild(newRow);
@@ -108,5 +106,57 @@ function createUI() {
 
 }
 
+function winChecker(){
+//     //Row Checker
+
+   for (var j = 0; j < size; j++){
+    var rowScore = 0
+    var zeroCount = 0;
+    for (var i = j; i < board.length; i = i + size){
+        
+         if (board[i] == 0){
+             break;
+         }
+         else {
+             rowScore = rowScore + board[i]
+              if(rowScore  == size ){
+                  console.log("x wins");
+              }
+              else if (rowScore == size * 2){
+                  console.log("o wins");
+              }
+             console.log(rowScore);
+         }
+            // zeroCount++;
+            // rowScore = rowScore + board[i];
+            // console.log(rowScore);
+        
+            // if (rowScore % size == 0 && zeroCount == 0){
+            //     console.log ("O wins");
+            // }
+            // else if (rowScore % (size * 2) == 0){
+            //     console.log ("X wins");
+            // }
+     
+     
+    }
+
+      //Col Checker
+    //   for (var i = j; i < size; i ++){
+        
+    //     if (board[i] !== 0){
+    //        console.log("Row checked");
+    //       console.log(i);
+    //  }
+    //  else {};
+    // }
+   }
+}
+//}
 createUI();
 popArray();
+
+function zeroCheck(){
+    if (board[i] == 0);
+    break;
+}
